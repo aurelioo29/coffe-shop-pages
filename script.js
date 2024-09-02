dd4efc452aba02ae667c6b6df8b96a614a50b661
aurelioo29 <aureliolucio5@gmail.com>
2024-09-02 09:39:26 +0700

    add Feature button search

--- script.js
+++ script.js
@@ -5,9 +5,21 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navCenter.classList.toggle("active");
 };
 
-const hamburger = document.querySelector("#hamburger-menu");
+const searchForm = document.querySelector(".search-form");
+const searchBarBox = document.querySelector("#search-box");
+document.querySelector("#search-btn").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBarBox.focus();
+  e.preventDefault();
+};
+
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+const searchBtn = document.querySelector("#search-btn");
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navCenter.contains(e.target)) {
+  if (!hamburgerMenu.contains(e.target) && !navCenter.contains(e.target)) {
     navCenter.classList.remove("active");
+  }
+  if (!searchBtn.contains(e.target) && !navCenter.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
