5643bce30d41122411214d01365e039e49e9f27e
aurelioo29 <aureliolucio5@gmail.com>
2024-09-02 11:10:02 +0700

    add Shopping cart feature

--- script.js
+++ script.js
@@ -13,13 +13,23 @@ document.querySelector("#search-btn").onclick = (e) => {
   e.preventDefault();
 };
 
+const shopCard = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-btn").onclick = (e) => {
+  shopCard.classList.toggle("active");
+  e.preventDefault();
+};
+
 const hamburgerMenu = document.querySelector("#hamburger-menu");
 const searchBtn = document.querySelector("#search-btn");
+const shopBtn = document.querySelector("#shopping-cart-btn");
 document.addEventListener("click", function (e) {
   if (!hamburgerMenu.contains(e.target) && !navCenter.contains(e.target)) {
     navCenter.classList.remove("active");
   }
   if (!searchBtn.contains(e.target) && !navCenter.contains(e.target)) {
     searchForm.classList.remove("active");
+  }
+  if (!shopBtn.contains(e.target) && !navCenter.contains(e.target)) {
+    shopCard.classList.remove("active");
   }
 });
